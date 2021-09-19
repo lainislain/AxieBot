0cfc0f5317ab4c58d2ca49f6e95046ee7dc016f2
illusionist99 <malaoui@student.1337.ma>
2021-09-19 22:26:40 +0100

    selecting axie by price

--- main.py
+++ main.py
@@ -4,36 +4,63 @@ import json
 
 # header 
 
-headers = {
-    'authority': 'graphql-gateway.axieinfinity.com',
-    'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
-    'accept': '*/*',
-    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SWQiOjM3OTE0NjQsImFjdGl2YXRlZCI6ZmFsc2UsInJvbmluQWRkcmVzcyI6IjB4YjMxZjM3OThhNDI3NjAzZjk1ZTI5NzcwOGU4NGM0MjRlY2NiYmEzMyIsImV0aEFkZHJlc3MiOm51bGwsImlhdCI6MTYzMTg3MjI1NiwiZXhwIjoxNjMyNDc3MDU2LCJpc3MiOiJBeGllSW5maW5pdHkifQ.tyj0F2ikySIPApOMjUVZTr3T5QvPCs57kg2v87IZTYo',
-    'sec-ch-ua-mobile': '?0',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
-    'content-type': 'application/json',
-    'origin': 'https://marketplace.axieinfinity.com',
-    'sec-fetch-site': 'same-site',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://marketplace.axieinfinity.com/',
-    'accept-language': 'en-US,en;q=0.9',
-}
 
-# variables and query // size is how many axies u want to get
+def run_query() :
+    """
+        gets lastest N axies on market
+    """
+    headers = {
+        'authority': 'graphql-gateway.axieinfinity.com',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+        'accept': '*/*',
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SWQiOjM3OTE0NjQsImFjdGl2YXRlZCI6ZmFsc2UsInJvbmluQWRkcmVzcyI6IjB4YjMxZjM3OThhNDI3NjAzZjk1ZTI5NzcwOGU4NGM0MjRlY2NiYmEzMyIsImV0aEFkZHJlc3MiOm51bGwsImlhdCI6MTYzMTg3MjI1NiwiZXhwIjoxNjMyNDc3MDU2LCJpc3MiOiJBeGllSW5maW5pdHkifQ.tyj0F2ikySIPApOMjUVZTr3T5QvPCs57kg2v87IZTYo',
+        'sec-ch-ua-mobile': '?0',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        'content-type': 'application/json',
+        'origin': 'https://marketplace.axieinfinity.com',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://marketplace.axieinfinity.com/',
+        'accept-language': 'en-US,en;q=0.9',
+    }
 
-data = '{"operationName":"GetAxieLatest","variables":{"from":0,"size":15,"sort":"Latest","auctionType":"Sale"},"query":"query GetAxieLatest($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\\n axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\\n total\\n results {\\n ...AxieRowData\\n __typename\\n }\\n __typename\\n }\\n}\\n\\nfragment AxieRowData on Axie {\\n id\\n image\\n class\\n name\\n genes\\n owner\\n class\\n stage\\n title\\n breedCount\\n level\\n parts {\\n ...AxiePart\\n __typename\\n }\\n stats {\\n ...AxieStats\\n __typename\\n }\\n auction {\\n ...AxieAuction\\n __typename\\n }\\n __typename\\n}\\n\\nfragment AxiePart on AxiePart {\\n id\\n name\\n class\\n type\\n specialGenes\\n stage\\n abilities {\\n ...AxieCardAbility\\n __typename\\n }\\n __typename\\n}\\n\\nfragment AxieCardAbility on AxieCardAbility {\\n id\\n name\\n attack\\n defense\\n energy\\n description\\n backgroundUrl\\n effectIconUrl\\n __typename\\n}\\n\\nfragment AxieStats on AxieStats {\\n hp\\n speed\\n skill\\n morale\\n __typename\\n}\\n\\nfragment AxieAuction on Auction {\\n startingPrice\\n endingPrice\\n startingTimestamp\\n endingTimestamp\\n duration\\n timeLeft\\n currentPrice\\n currentPriceUSD\\n suggestedPrice\\n seller\\n listingIndex\\n state\\n __typename\\n}\\n"}'
+    # variables and query // size is how many axies u want to get
 
-# sending Post request with data and headers
+    data = '{"operationName":"GetAxieLatest","variables":{"from":0,"size":15,"sort":"Latest","auctionType":"Sale"},"query":"query GetAxieLatest($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\\n axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\\n total\\n results {\\n ...AxieRowData\\n __typename\\n }\\n __typename\\n }\\n}\\n\\nfragment AxieRowData on Axie {\\n id\\n image\\n class\\n name\\n genes\\n owner\\n class\\n stage\\n title\\n breedCount\\n level\\n parts {\\n ...AxiePart\\n __typename\\n }\\n stats {\\n ...AxieStats\\n __typename\\n }\\n auction {\\n ...AxieAuction\\n __typename\\n }\\n __typename\\n}\\n\\nfragment AxiePart on AxiePart {\\n id\\n name\\n class\\n type\\n specialGenes\\n stage\\n abilities {\\n ...AxieCardAbility\\n __typename\\n }\\n __typename\\n}\\n\\nfragment AxieCardAbility on AxieCardAbility {\\n id\\n name\\n attack\\n defense\\n energy\\n description\\n backgroundUrl\\n effectIconUrl\\n __typename\\n}\\n\\nfragment AxieStats on AxieStats {\\n hp\\n speed\\n skill\\n morale\\n __typename\\n}\\n\\nfragment AxieAuction on Auction {\\n startingPrice\\n endingPrice\\n startingTimestamp\\n endingTimestamp\\n duration\\n timeLeft\\n currentPrice\\n currentPriceUSD\\n suggestedPrice\\n seller\\n listingIndex\\n state\\n __typename\\n}\\n"}'
 
-response = requests.post('https://graphql-gateway.axieinfinity.com/graphql', headers=headers, data=data)
+    # sending Post request with data and headers
 
-# to json
-response = response.json()
+    response = requests.post('https://graphql-gateway.axieinfinity.com/graphql', headers=headers, data=data)
 
+    # to json
+    response = response.json()
+    return response
+
+
+#def buy_axie():
+
+
+
+def select_axie(data, priceUsd):
+
+    # filtering axies data
+    # each axie have it's
+    # ['id', 'image', 'class', 'name', 'genes', 'owner', 'stage', 'title', 'breedCount', 'level', 'parts', 'stats', 'auction', '__typename']
+    data = data['data']['axies']['results']
+    for axies in data:
+        #if float(axies['auction']['currentPriceUSD']) <= priceUsd:
+        print("Axie {} with current price of {} Usd, seller is : {}".format(axies['id'], axies['auction']['currentPriceUSD'], axies['auction']['seller']))
+            #buy_axie()
+
+
+
+data = run_query()
+select_axie(data, 500)
+#buy_axie()
 
 # printing data to screen 
-print(response)
+#print(data)
 
 
 # how to access Data and check cards  + price and purity + breed 
