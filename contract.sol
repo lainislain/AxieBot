3018f226a36e745bd7661aaa32b2c6d3173e968c
Mohamed Alaoui <malaoui@e0r1p1.1337.ma>
2021-09-20 16:42:28 +0100

    ?

--- contract.sol
+++ contract.sol
@@ -3,3 +3,11 @@
 function settleAuction(address,address,uint256,uint256,uint256) external {
 
 }
+// hadxi useless 
+//Function:settleAuction(address,address,uint256,uint256,uint256)
+// Arguments:
+// [0]-[_seller]: 0x9a105e4a4cbcdc00bcbde822f55af75890bfbb50
+// [1]-[_token]: 0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5
+// [2]-[_bidAmount]: 98000000000000000
+// [3]-[_listingIndex]: 102839
+// [4]-[_listingState]: 86018417604930940163621651514773807212604807070510614929907522643158481769800
